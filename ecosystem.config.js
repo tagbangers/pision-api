159d0d6863a6b2147356085d381298508163bcdb
sasaki <sasaki@tagbangers.co.jp>
2019-09-24 01:01:03 +0900

    Setup PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,30 +1,35 @@
 module.exports = {
-  apps : [{
-    name: 'API',
-    script: 'app.js',
+  apps: [
+    {
+      name: "pision-api",
+      script: "app.js",
 
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      args: "one two",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
     }
-  }],
+  ],
 
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+  deploy: {
+    production: {
+      user: "ec2-user",
+      host: "ec2-3-113-22-35.ap-northeast-1.compute.amazonaws.com",
+      key: "~/.ssh/pision-tagbangers.pem",
+      ref: "origin/master",
+      repo: "git@github.com:tagbangers/pision-api.git",
+      ssh_options: "StrictHostKeyChecking=no",
+      path: "/home/ec2-user/pision-api",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production"
     }
   }
 };
